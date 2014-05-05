72135998b106abc31a04dbeb1bcc8a01f90b1cce
Gilles Scokart <gscokart@gmail.com>
2014-05-05 16:35:24 +0200

    first haml file

--- app/netmento.rb
+++ app/netmento.rb
@@ -5,13 +5,19 @@ require 'sinatra'
 enable :session
 set :public_folder, File.dirname(__FILE__) + '/../static/'
 
+use Rack::Auth::Basic, "Netmento" do |username, password|
+  ([username, password] == ['admin', 'admin']) or ([username, password] == ['other', 'other'])
+end
+
 
 get '/' do
   redirect '/netmento'
 end
 
 get '/netmento' do
-  "This is the home page"
+  #TODO understand how env is accessible/defined here, and if it is thread safe
+  #"This is the home page = #{env['REMOTE_USER']}"
+  haml :home, :format => :html5
 end
 
 get '/netmento/:user' do
